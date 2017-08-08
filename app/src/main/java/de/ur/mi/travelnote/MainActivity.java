4a1e0f5f2587802975465dd616444f80ea797496
Andreas <androbey@yahoo.com>
2017-08-08 22:54:58 +0200

    Initial commit

---
+++ app/src/main/java/de/ur/mi/travelnote/MainActivity.java
@@ -0,0 +1,13 @@
+package de.ur.mi.travelnote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
