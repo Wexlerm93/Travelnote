fef900cfd56636b7eb90ef1e159848dda32b6267
Wexlerm93 <30842659+Wexlerm93@users.noreply.github.com>
2017-08-09 13:46:19 +0200

    test

--- app/src/main/java/de/ur/mi/travelnote/MainActivity.java
+++ app/src/main/java/de/ur/mi/travelnote/MainActivity.java
@@ -2,6 +2,9 @@ package de.ur.mi.travelnote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
